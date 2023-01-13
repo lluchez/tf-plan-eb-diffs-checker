11283d5a12dceea0601594419ef9af19fe50c76a
Lionel Luchez <lionel.luchez@snapsheet.me>
2023-01-13 11:01:11 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tf-plan-eb-diffs-checker
 Simply UI to show diffs on a Terraform plan for ElasticBeanstalk changes
+
+
+This simple project uses no dependencies and provide a minimal UI for you to paste your TF plan.
+Automatically, the parser will extract all declarations and will build a grid to show differences, easing the task to review EB plans.
---
+++ script.js
@@ -0,0 +1,54 @@
+// NOTE: regexp created here: https://regex101.com/r/sKZjA8/1
+const PARSING_REGEXP = /\s(?<type>[\+\-])\s+setting\s\{\s+[\+\-]\sname\s+=\s\"(?<name>[^"]+)\"(?:\s\->\snull)?\s+[\+\-]\snamespace\s=\s\"[^"]+\"(?:\s\->\snull)?\s+[\+\-]\svalue\s+=\s\"(?<value>[^"]+)\"(?:\s\->\snull)?/g;
+
+
+function parseText(text) {
+  const listedProps = new Set(), newProps = [], prevProps = [];
+  for (const match of text.matchAll(PARSING_REGEXP)) {
+    const g = match.groups, type = g['type'], name = g['name'], value = g['value'], list = type == '-' ? prevProps : newProps;
+    listedProps.add(name);
+    list[name] = value;
+  }
+  const allDiffs = [];
+  for (const propName of listedProps) {
+    allDiffs.push({
+      name: propName,
+      oldValue: prevProps[propName],
+      newValue: newProps[propName]
+    })
+  }
+  console.log(listedProps, newProps, prevProps);
+  return allDiffs;
+}
+
+function addRowToTable(table, cellsText) {
+  const row = table.insertRow(-1);
+  cellsText.forEach(text => addCellToRow(row, text));
+}
+
+function addCellToRow(row, text) {
+  const cell = row.insertCell(-1);
+  cell.innerHTML = `<pre>${text}</pre>`;
+}
+
+function handleTerraformPlanChanged(event) {
+  const allProps = parseText(event.target.value);
+  const diffProps = allProps.filter(x => x.oldValue != x.newValue);
+  const resultsTableBody = document.getElementById('results').getElementsByTagName('tbody')[0];
+  if (diffProps.length) {
+    resultsTableBody.innerHTML = ''; // clear old rows
+    diffProps.forEach(({name, oldValue, newValue}) => addRowToTable(resultsTableBody, [name, oldValue, newValue]))
+  }
+  else if (allProps.length) {
+    resultsTableBody.innerHTML = '<tr><td colspan="3" class="no-results">No diffs found</td></tr>';
+  } else {
+    resultsTableBody.innerHTML = '<tr><td colspan="3" class="no-results">Please enter a valid TF plan</td></tr>';
+  }
+}
+
+window.addEventListener("load", () => {
+  const textbox = document.getElementById('diffs');
+  textbox.addEventListener('keyup', handleTerraformPlanChanged);
+  textbox.focus();
+});
+
